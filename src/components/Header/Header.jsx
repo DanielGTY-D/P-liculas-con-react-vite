bc64eccb6bd85e5c224b31794c56c4895118300a
Yael Torres <domestika23@gmail.com>
2024-01-23 19:33:06 -0600

    Comenzando con el css a Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
+import "../header/Header.css"
 function Header() {
     return (
         <>
-            
+            <div className="header">
+                <h2>PeliNet.all</h2>
+                
+                <div>
+                    <img src="../../../public/icons/lupa.svg" width={"30px"} height={"30px"} alt="Buscador de peliculas" />
+                    <input type="text" placeholder="Busca tu Pélicula Favorita"/>
+                </div>
+            </div>
         </>
     )
 }
